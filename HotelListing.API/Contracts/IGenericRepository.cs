be046ae5af2df4524d647944f693893c0043fad2
gokcaneren <gokcann01@gmail.com>
2022-08-25 14:10:04 +0300

    Refactoring to repository pattern

---
+++ HotelListing.API/Contracts/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelListing.API.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<bool> IsExistAsync(int id);
+    }
+}
---
+++ HotelListing.API/Repository/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly HotelListingDbContext _context;
+
+        public GenericRepository(HotelListingDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<bool> IsExistAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            return entity != null;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
