dd47b2917d5c4ae13c34fe7ae2633f31a8dec4d5
gokcaneren <gokcann01@gmail.com>
2022-08-28 14:34:33 +0300

    registration endpoint is completed

---
+++ HotelListing.API/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Models.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAuthManager _authManager;
+
+        public AccountController(IAuthManager authManager)
+        {
+            _authManager = authManager;
+        }
+
+        // Post: api/Account/register
+        [HttpPost]
+        [Route("register")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Register([FromBody] ApiUserDto apiUserDto)
+        {
+            var errors = await _authManager.Register(apiUserDto);
+
+            if (errors.Any())
+            {
+                foreach (var error in errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+            return Ok();
+        }
+    }
+}
--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddAutoMapper(typeof(MapperConfig));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 builder.Services.AddScoped<IHotelRepository, HotelRepository>();
+builder.Services.AddScoped<IAuthManager, AuthManager>();
 
 var app = builder.Build();
 
